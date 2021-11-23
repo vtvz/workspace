83481745053a7c845c293c977928411a253403c7
Vitaly Zaslavsky <vtvz@pm.me>
2021-11-23 13:56:57 +0300

    More advanced check update scripts. Include all tools I love

--- .meta/bin/check-updates.sh
+++ .meta/bin/check-updates.sh
@@ -3,19 +3,39 @@
 set -euo pipefail
 
 return_code=0
+function check {
+    repo=$1
+    current=$2
+    latest=$3
 
-for component in $(echo $VERSIONS | jq -cM 'to_entries | .[]'); do
-  repo=$(echo $component | jq -r '.key')
-  current=$(echo $component | jq -r '.value')
+    if [[ "$latest" != "$current" ]]; then
+      echo "* Update for $repo available $latest (current $current)"
+      return_code=$((return_code+1))
+    else
+      echo "- $repo up to date ($current)"
+    fi
+}
+
+for component in $(echo "$VERSIONS" | jq -cM 'to_entries | .[]'); do
+  repo=$(echo "$component" | jq -r '.key')
+  current=$(echo "$component" | jq -r '.value')
   latest=$(curl -s "https://api.github.com/repos/${repo}/releases/latest" | jq -r '.tag_name')
 
-  if [[ "$latest" != "$current" ]]; then
-    echo "* Update for $repo available $latest (current $current)"
-
-    return_code=1
-  else
-    echo "- $repo up to date ($current)"
-  fi
+  check "$repo" "$current" "$latest"
 done
+
+# kubectl
+current=$(kubectl version --client -o json | jq -r '.clientVersion.gitVersion')
+latest=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
+check "kubernetes/kubectl" "$current" "$latest"
+
+# golang
+if [[ -n $GOLANG_VERSION ]]; then
+  current="go$GOLANG_VERSION"
+  latest=$(curl -s "https://go.dev/VERSION?m=text")
+  check "golang/go" "$current" "$latest"
+fi
+
+op update
 
 exit $return_code
